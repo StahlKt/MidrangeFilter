6e6dd9b9fa7e529490aceb7ed9f3f09b797ccc25
StahlKt <46341259+StahlKt@users.noreply.github.com>
2025-01-03 19:50:13 +0100

    Update example_script.R
    fixed typo and added comments

--- MidrangeFilter/example_script.R
+++ MidrangeFilter/example_script.R
@@ -1,38 +1,51 @@
+#load data.table package
 library(data.table)
 
 
 
-# source the docs with the functions
-#and load the toy data set
-#inluding the list of positions in the scaffold
+# source the script with the functions
+source("functionsMidrangeFilter.R")
 
+#and load the data sets in the example file folder:
+
+#this is a data table with imputed SNPs
 imputed_snps<-readRDS("example_data/imputed_snps.RDS")
-snps_scaffold<-readRDS("example_data/snps_scaffold.RDS")
-
+#check properties
 names(imputed_snps)
 summary(imputed_snps)
 
+
+#this is a data table listing SNPs which were not imputed but typed by the SNP array
+snps_scaffold<-readRDS("example_data/snps_scaffold.RDS")
+#check properties
 names(snps_scaffold)
 summary(snps_scaffold)
 
 
-#determine the spikes in the data set
+#determine the spikes in the data set with the determine_spikes function from the other script.
+#the example files have names matching the functions expectation,
+#but all column names may be specified as an input parameter
 spike_table<-determine_spikes(imputed_snps)
+#result table hold one line per detected SNPs
 spike_table
 
-#validate best guess spikes in dosage to determine the type of spike:
-#best guess specific or not
+#check best guess spikes to determine in which case they fall into:
+#are they present in dosage as well or ar they specific to best guess
 spike_table_validated<-validate_bg(spike_table)
+#this new table has an extra column that specifies the type of spike
 spike_table_validated
 
 #remove spikes that contain significant SNPs, that were not imputed
+#these spikes are presumed to be true spikes and do not need further filtering
 spike_table_validated_checked<-check_scaffold(spike_table_validated, snps_scaffold, imputed_snps)
 spike_table_validated_checked
 
-#run the Midrange Filter to keep or discard spikes
+#run the Midrange Filter to keep or discard spikes:
+
 #decisions listed for each SNP
-result<-midrange_filter(test_snp_table, spike_table_validated_checked, quality.col = "BR2_MIN")
+result<-midrange_filter(imputed_snps, spike_table_validated_checked, quality.col = "BR2_MIN")
 result
+
 #decisions listed for each spike
-result_condensed<-midrange_filter(test_snp_table, spike_table_validated_checked, quality.col = "BR2_MIN", condensed.return.table = TRUE)
+result_condensed<-midrange_filter(imputed_snps, spike_table_validated_checked, quality.col = "BR2_MIN", condensed.return.table = TRUE)
 result_condensed
